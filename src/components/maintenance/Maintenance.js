c9aec06510c30fef188ccba6ca6bdf3adc155f46
sydneywait <sydneywait@gmail.com>
2019-04-01 13:07:11 -0400

    added basic structure for maintenance request page

--- src/components/maintenance/Maintenance.js
+++ src/components/maintenance/Maintenance.js
@@ -1,9 +1,24 @@
 import React, { Component } from "react";
+import "./Maintenance.css"
 
 export default class Maintenance extends Component{
 
 
-    render (){
-        return <h1>this is the maintenance page</h1>
+
+    render() {
+        return (
+            <React.Fragment>
+                <h2>Maintenance Requests</h2>
+                <div className="maint-cont">
+                    <div className="maint-req-form">
+
+                    </div>
+                    <div className="maint-req-list">
+                        list
+                    </div>
+                </div>
+
+            </React.Fragment>
+        )
     }
 }
--- src/components/maintenance/MaintenanceRequest.js
+++ src/components/maintenance/MaintenanceRequest.js
@@ -1,9 +1,25 @@
 import React, { Component } from "react";
+import "./Maintenance.css"
 
-export default class MaintenanceRequest extends Component{
+export default class MaintenanceRequest extends Component {
 
 
-    render (){
-        return <h1>this is the maintenance request page</h1>
+    render() {
+        return (
+            <React.Fragment>
+                <div className="maint-header">
+                <h1>Maintenance Requests</h1>
+                </div>
+                <div className="maint-cont">
+                    <div className="maint-req-form">
+                        form
+                    </div>
+                    <div className="maint-req-list">
+                        list
+                    </div>
+                </div>
+
+            </React.Fragment>
+        )
     }
 }
